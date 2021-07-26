b3c6b32a64babae957ef84d752ba6596183bf0b1
pradeepkambham <pradeep.kambham@nbcuni.com>
2021-07-26 16:58:32 -0500

    jjssj

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region                  = "us-east-1"
+  shared_credentials_file = "/Users/prade/.aws/credentials"
+  profile                 = "customprofile"
+}
+
+
+
+module "pradeep_webserver" {
+  source = "/modules/ec2"
+  vpc_id= aws_vpc.main.id
+  vpc_cidr_block="10.0.0.0/16"
+  subnet_cidr_block="10.0.1.0/24"
+  ami="ami-0dc2d3e4c0f9ebd18"
+  instance_type="t2.micro"
+  webserver_name="pradeep"
+}
