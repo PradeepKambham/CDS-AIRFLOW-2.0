31121a6bd5f2054e8a1b4e8b92c0cbcdebee36c3
pradeepkambham <pradeep.kambham@nbcuni.com>
2021-07-27 17:33:55 -0500

    jjssj

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "main" {
-  cidr_block =var.vpc_cidr
+  cidr_block ="10.0.0.0/16"
 
 }
 resource "aws_subnet" "webserver" {
@@ -22,3 +22,6 @@ resource "aws_instance" "web" {
   }
 }
 
+output "vpc_id" {
+  value = ""aws_vpc.main.id
+}
--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -2,9 +2,7 @@ variable "vpc_id" {
   type=string
 }
 
-variable "vpc_cidr" {
-  
-}
+
 
 variable "subnet_cidr_block" {
    type = string
